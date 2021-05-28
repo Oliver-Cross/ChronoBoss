54db02a05d6b737980e151507132d5d4b83879e9
jemimalomax <j.lomaxsawyers@gmail.com>
2021-05-28 14:22:31 +1200

    database test fragment in progress

--- app/src/main/java/com/example/chronoboss/database/DatabaseTestFragment.kt
+++ app/src/main/java/com/example/chronoboss/database/DatabaseTestFragment.kt
@@ -1,10 +1,13 @@
 package com.example.chronoboss.database
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.chronoboss.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,24 +21,46 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DatabaseTestFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var mDayViewModel: DayViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_database_test, container, false)
+        val view =  inflater.inflate(R.layout.fragment_database_test, container, false)
+
+        mDayViewModel = ViewModelProvider(this).get(DayViewModel::class.java)
+
+        view.add_btn.setOnClickListener{
+            insertDataToDatabase()
+        }
+
+        return view
+
+
+
     }
 
+    private fun insertDataToDatabase() {
+        val timeWasted = addTimeWasted_et.text
+        val timeLimit = addTimeLimit_et.text
+        val application = addApplication.text.toString()
+
+        if(inputCheck(application, timeWasted, timeLimit)){
+            //Create day object
+            val day = Day(0, )
+        }
+    }
+
+    private fun inputCheck(application: String, timeWasted: Editable, timeLimit: Editable): Boolean{
+        return !(TextUtils.isEmpty(application) && timeWasted.isEmpty() && timeLimit.isEmpty())
+    }
+
+
 }
+
