a27d9fd04591dc6b59d69f9504b2a503d66c5f48
Oliver <swampyjnr@gmail.com>
2021-06-03 09:49:12 +1200

    graph should now display with dummy data

--- app/src/main/java/com/example/chronoboss/StatsFragment.kt
+++ app/src/main/java/com/example/chronoboss/StatsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.chronoboss
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,9 @@ import androidx.databinding.DataBindingUtil
 import com.example.chronoboss.databinding.FragmentProgressBinding
 import com.example.chronoboss.databinding.FragmentStatsBinding
 import com.github.mikephil.charting.charts.*
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,8 +39,10 @@ class StatsFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ResourceAsColor")
     fun setLineChartData(){
 
+        val line_chart : LineChart = LineChart(context)
         val xvalues = ArrayList<String>()
         xvalues.add("Monday")
         xvalues.add("Tuesday")
@@ -46,6 +52,26 @@ class StatsFragment : Fragment() {
         xvalues.add("Saturday")
         xvalues.add("Sunday")
 
+        /* Line entry: requires an object Entry, has constructors:
+                x: float, y: float, and option for a drawable to make it pretty later
+         */
+        val lineEntry: ArrayList<Entry> = ArrayList()
+        lineEntry.add(Entry(0f, 90F))
+        lineEntry.add(Entry(1f, 95F))
+        lineEntry.add(Entry(2f, 75F))
+        lineEntry.add(Entry(3f, 80F))
+        lineEntry.add(Entry(4f, 85F))
+        lineEntry.add(Entry(5f, 90F))
+        lineEntry.add(Entry(6f, 95F))
+
+        val progressDataSet = LineDataSet(lineEntry, "Progress of the Week!")
+        progressDataSet.color = resources.getColor(R.color.teal_200)
+
+        val data = LineData(progressDataSet)
+
+        line_chart.data = data
+        line_chart.setBackgroundColor(R.color.white)
+        line_chart.animateXY(2000, 2000)
 
 
     }
