7c11e47f8e02ac14b58a0739f753b9e7ffbc0f65
taya-nicholas <taya.nicholas@outlook.com>
2021-06-03 13:33:19 +1200

    Fixed adapter import error.

--- app/src/main/java/com/example/chronoboss/FragmentAdapter.kt
+++ app/src/main/java/com/example/chronoboss/FragmentAdapter.kt
@@ -4,6 +4,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.chronoboss.homeFragment.HomeFragment
+import com.example.chronoboss.progressFragment.ProgressFragment
+import com.example.chronoboss.settingsFragment.SettingsFragment
+import com.example.chronoboss.statsFragment.StatsFragment
 
 class FragmentAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
