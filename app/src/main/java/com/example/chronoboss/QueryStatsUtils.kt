fd4d619745dc66d35d70cb2160506bfce9279ebc
jemimalomax <j.lomaxsawyers@gmail.com>
2021-06-04 21:47:09 +1200

    comments added

--- app/src/main/java/com/example/chronoboss/QueryStatsUtils.kt
+++ app/src/main/java/com/example/chronoboss/QueryStatsUtils.kt
@@ -11,17 +11,25 @@ import android.os.Build
 import android.provider.Settings
 import androidx.annotation.RequiresApi
 
-
+/** utility class providing functionality relating to queries to the OS
+ *
+ */
 class QueryStatsUtils {
 
     lateinit var topPackage:UsageStats
     //val targetPackage: UsageStats? = null
 
+    /** function to request access to usage statistics which is called upon setup
+     *
+     */
     fun requestUsageStatsPermission(activity: Activity) {
         val intent: Intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
         activity.startActivity(intent)
     }
 
+    /** function to check whether access to usage statistics has already been allowed
+     *
+     */
     fun hasStatsPermission(context: Context, activity: Activity): Boolean {
 
         val applicationInfo: ApplicationInfo? =
@@ -38,6 +46,10 @@ class QueryStatsUtils {
         return (mode == AppOpsManager.MODE_ALLOWED)
     }
 
+    /** function to get list of usage statistics
+     * returns a list where each package has available information that can
+     * be accessed such as package name
+     */
     fun getStats(context: Context?): List<UsageStats> {
         val statsManager =
             context?.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
@@ -49,6 +61,9 @@ class QueryStatsUtils {
         return usageStats
     }
 
+    /** function for getting wasted time with the intention of being used
+     * for the database to pull stats by day
+     */
     fun getTimeWasted(context:Context, beginTime:Long, endTime:Long):Long{
         val targetPackageName:String = "com.android.chrome"
         var timeWasted:Long = 0
@@ -89,6 +104,7 @@ class QueryStatsUtils {
         return targetPackage?.packageName
     } */
 
+    /** function that pulls
     fun getTopPackage(context: Context?): UsageStats {
         val usageStats = getStats(context)
         var timeUsed: Long = 0
